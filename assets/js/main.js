a9e5f18c6ab20537dbce82ebe5cc4be9e3cdde06
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-08-29 13:43:32 +0200

    add dice game
    started email ex

---
+++ assets/js/main.js
@@ -0,0 +1,101 @@
+/* 
+
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+*/
+
+// random number for player and pc 
+
+const playerNumber = Math.floor(Math.random() * 6) + 1;
+
+const pcNumber = Math.floor(Math.random() * 6) + 1;
+
+console.log(pcNumber, playerNumber);
+
+let winner;
+
+// who is the winner? 
+
+if (playerNumber > pcNumber) {
+
+    winner  = 'Player'
+    console.log(`${winner} has won!!`);
+
+} else if (playerNumber < pcNumber) {
+
+    winner = 'Pc'
+    console.log(`${winner} has won!!`);
+
+} else {
+
+    console.log('Even, no one wins');
+
+}
+
+
+
+/* 
+
+Mail
+Chiedi all'utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+Bonus
+Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
+
+*/
+
+// create array of emails 
+
+const usersEmail = [
+    'email_1@email.it',
+    'email_2@email.it',
+    'email_3@email.it',
+    'email_4@email.it',
+    'email_5@email.it',
+    'email_6@email.it',
+    'email_7@email.it',
+    'email_8@email.it',
+]
+
+// ask the email to user 
+
+let joinEmail = document.getElementById('emailInput');
+
+let canEnter = false;
+
+// list check
+
+const submit = document.querySelector('button')
+
+submit.addEventListener('click', function() {
+
+    for (i = 0; i < usersEmail.length; i++) {
+
+        if (joinEmail === usersEmail[i]) {
+    
+            canEnter = true;
+    
+        }
+    
+    }
+    
+    if (canEnter) {
+    
+        console.log('Welcome host');
+    
+    } else {
+    
+        console.log('denied access');
+    
+    }
+
+})
+
+
+
+
+
+
+
+
+
